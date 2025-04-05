737b385700302cab805b0e225754bb7515ba3cab
yonghun <asqwklop12@gmail.com>
2025-04-06 07:30:40 +0900

    Test: 소모임 단일 조회 테스트

--- gathering/src/test/java/com/sparta/moim/gathering/presentation/contoller/GatheringControllerTest.java
+++ gathering/src/test/java/com/sparta/moim/gathering/presentation/contoller/GatheringControllerTest.java
@@ -2,16 +2,17 @@ package com.sparta.moim.gathering.presentation.contoller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.moim.gathering.application.dto.command.CreateGatheringCommand;
 import com.sparta.moim.gathering.application.dto.query.CreateGatheringQuery;
+import com.sparta.moim.gathering.application.dto.query.GetGatheringQuery;
 import com.sparta.moim.gathering.application.service.GatheringService;
 import com.sparta.moim.gathering.presentation.dto.request.UpdateGatheringRequest;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -81,7 +82,6 @@ class GatheringControllerTest {
         false
     );
 
-
     // when & then
     mockMvc.perform(put("/api/v1/gathering/{gatheringId}", gatheringId)
             .contentType(MediaType.APPLICATION_JSON)
@@ -90,5 +90,40 @@ class GatheringControllerTest {
             .header("X-User-Role", "USER")
             .header("X-User-ID", UUID.randomUUID().toString()))
         .andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("게더링 단일 조회 성공")
+  void getGathering_success() throws Exception {
+    // given
+    UUID gatheringId = UUID.randomUUID();
+    GetGatheringQuery response = new GetGatheringQuery(
+        gatheringId,
+        "org123",
+        "테스트 모임",
+        "테스트유저",
+        10,
+        true,
+        LocalDateTime.now(),
+        "아이디",
+        LocalDateTime.now(),
+        "아이디"
+    );
+
+    when(gatheringService.getGathering(any()))
+        .thenReturn(response);
+
+    // when & then
+    mockMvc.perform(get("/api/v1/gathering/{gatheringId}", gatheringId)
+            .header("X-User-Name", "테스트유저")
+            .header("X-User-Role", "USER")
+            .header("X-User-ID", UUID.randomUUID().toString()))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.gatheringId").value(gatheringId.toString()))
+        .andExpect(jsonPath("$.organizationId").value("org123"))
+        .andExpect(jsonPath("$.name").value("테스트 모임"))
+        .andExpect(jsonPath("$.owner").value("테스트유저"))
+        .andExpect(jsonPath("$.count").value(10))
+        .andExpect(jsonPath("$.status").value(true));
   }
 }
