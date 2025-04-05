b9ddcb271051d96c1eaaf40a469d4fb37fa80d26
yonghun <asqwklop12@gmail.com>
2025-04-05 16:49:33 +0900

    Feat: 구조 init

---
+++ gathering/src/test/java/com/sparta/moim/gathering/GatheringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.moim.gathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
