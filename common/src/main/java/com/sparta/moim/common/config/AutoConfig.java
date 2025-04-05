cd672a6accb85918a0ba6f4eab6de7c4a99bfa80
cchoijjinyoung <cgy7127@gmail.com>
2025-04-06 00:03:26 +0900

    Feat: 공통 Config init

---
+++ common/src/main/java/com/sparta/moim/common/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.moim.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sparta.moim.common")
+public class AutoConfig {
+}
