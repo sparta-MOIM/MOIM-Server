26a2561cb944baed176fb461ffdf397a112eb748
yonghun <asqwklop12@gmail.com>
2025-04-07 00:48:02 +0900

    Chore: GatheringRepository 추가

--- gathering/src/main/java/com/sparta/moim/gathering/infrastruct/repository/JpaGatheringRepository.java
+++ gathering/src/main/java/com/sparta/moim/gathering/infrastruct/repository/JpaGatheringRepository.java
@@ -1,7 +1,7 @@
 package com.sparta.moim.gathering.infrastruct.repository;
 
 import com.sparta.moim.gathering.domain.entity.Gathering;
-import com.sparta.moim.gathering.domain.entity.repository.GatheringRepository;
+import com.sparta.moim.gathering.domain.repository.GatheringRepository;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
