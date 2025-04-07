c6cb04568489f0c3e958c33a4d3d27b6156dd799
yonghun <asqwklop12@gmail.com>
2025-04-08 02:31:18 +0900

    Refactor: @NotNull 처리

--- gathering/src/main/java/com/sparta/moim/gathering/presentation/dto/request/CreateGatheringRequest.java
+++ gathering/src/main/java/com/sparta/moim/gathering/presentation/dto/request/CreateGatheringRequest.java
@@ -1,14 +1,16 @@
 package com.sparta.moim.gathering.presentation.dto.request;
 
 import com.sparta.moim.gathering.application.dto.command.CreateGatheringCommand;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 public record CreateGatheringRequest(
-    String organizationId,
-    String name,
-    String owner,
+    @NotNull String organizationId,
+    @NotNull String name,
+    @NotNull String owner,
     @Positive int count,
-    boolean status
+    @DefaultValue(value = "true") boolean status
 ) {
   public CreateGatheringCommand toCommand() {
     return new CreateGatheringCommand(organizationId, name, owner, count, status);
--- gathering/src/main/java/com/sparta/moim/gathering/presentation/dto/request/UpdateGatheringRequest.java
+++ gathering/src/main/java/com/sparta/moim/gathering/presentation/dto/request/UpdateGatheringRequest.java
@@ -2,13 +2,14 @@ package com.sparta.moim.gathering.presentation.dto.request;
 
 
 import com.sparta.moim.gathering.application.dto.command.UpdateGatheringCommand;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.util.UUID;
 
 public record UpdateGatheringRequest(
-    String name,
+    @NotNull String name,
     @PositiveOrZero int count,
-    Boolean status
+    @NotNull Boolean status
 ) {
   public UpdateGatheringCommand toCommand(UUID gatheringId) {
     return new UpdateGatheringCommand(gatheringId, name, count, status);
