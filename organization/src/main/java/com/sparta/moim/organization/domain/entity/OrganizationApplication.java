e59b0d05b4121e5ba2d5d6e98fd0a2fd9e8da6bb
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-04-09 21:45:06 +0900

    feat: 모임 신청서 entity 작성

---
+++ organization/src/main/java/com/sparta/moim/organization/domain/entity/OrganizationApplication.java
@@ -0,0 +1,47 @@
+package com.sparta.moim.organization.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Types;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Where(clause = "deleted_at IS NULL")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "p_organization_application")
+public class OrganizationApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @UuidGenerator
+    @JdbcTypeCode(Types.VARCHAR)
+    @Column(name = "tracking_id", length = 36, nullable = false, unique = true)
+    private UUID trackingId;
+
+    @JdbcTypeCode(Types.VARCHAR)
+    @Column(name="user_tracking_id",length = 36, nullable = false, unique = true)
+    private UUID userTrackingId;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String content;
+}
---
+++ organization/src/main/java/com/sparta/moim/organization/presentation/controller/OrganizationApplicationController.java
@@ -0,0 +1,16 @@
+package com.sparta.moim.organization.presentation.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/organizations/{organizationTrackingId}/applications")
+@RequiredArgsConstructor
+public class OrganizationApplicationController {
+
+    @PostMapping
+    
+}
