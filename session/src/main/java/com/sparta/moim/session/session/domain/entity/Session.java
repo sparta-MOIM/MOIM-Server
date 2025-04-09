b134304ec627e7bdbab66b18d2b709159f03c4a4
yonghun <asqwklop12@gmail.com>
2025-04-10 04:12:03 +0900

    Feat: Session 엔티티 생성

---
+++ session/src/main/java/com/sparta/moim/session/session/domain/entity/Session.java
@@ -0,0 +1,66 @@
+package com.sparta.moim.session.session.domain.entity;
+
+import com.sparta.moim.session.session.domain.enums.SessionStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Types;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Getter
+@Table(name = "p_session")
+public class Session {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+
+  @Column(nullable = false, length = 100)
+  private String organizationId;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String title;
+
+  @Column(nullable = false, length = 100)
+  private String publisher;
+
+  private int count;
+
+  private LocalDateTime openTime;
+
+  private LocalDateTime closeTime;
+
+  @Enumerated(EnumType.STRING)
+  private SessionStatus status;
+
+
+  private LocalDateTime applyTime;
+
+  private LocalDateTime confirmTime;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String reason;
+
+  @UuidGenerator
+  @JdbcTypeCode(Types.VARCHAR)
+  @Column(length = 36, nullable = false, unique = true)
+  private UUID trackingId;
+
+}
