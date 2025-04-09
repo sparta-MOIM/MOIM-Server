82a5b292f39dfa0e743321a4b7e8c4f334a98a28
yonghun <asqwklop12@gmail.com>
2025-04-09 16:49:32 +0900

    Feat: 에러코드 공통화 -> 각 도메인에서 errorcode와 exception 정의해서 사용할 수 있음

--- common/src/main/java/com/sparta/moim/common/exception/BaseException.java
+++ common/src/main/java/com/sparta/moim/common/exception/BaseException.java
@@ -2,6 +2,7 @@ package com.sparta.moim.common.exception;
 
 import static com.sparta.moim.common.response.Code.INTERNAL_SERVER_ERROR;
 
+import com.sparta.moim.common.response.BaseError;
 import com.sparta.moim.common.response.Code;
 import lombok.Getter;
 
@@ -9,53 +10,53 @@ import lombok.Getter;
 // 일괄된 커스텀 에러를 생성해주기 위한 BaseException
 public class BaseException extends RuntimeException {
 
-    private final Code errorCode;
+    private final BaseError baseError;
 
     // 기본 생성자에서는 일반적인 에러 코드를 사용
     public BaseException() {
         super(INTERNAL_SERVER_ERROR.getMessage());
-        this.errorCode = INTERNAL_SERVER_ERROR;
+        this.baseError = INTERNAL_SERVER_ERROR;
     }
 
     // 에러 메시지를 받는 생성자
     public BaseException(String message) {
         super(message);
-        this.errorCode = INTERNAL_SERVER_ERROR;
+        this.baseError = INTERNAL_SERVER_ERROR;
     }
 
     // 에러 메시지와 원인을 받는 생성자
     public BaseException(String message, Throwable cause) {
         super(message, cause);
-        this.errorCode = INTERNAL_SERVER_ERROR;
+        this.baseError = INTERNAL_SERVER_ERROR;
     }
 
     // 원인만을 받는 생성자
     public BaseException(Throwable cause) {
         super(cause);
-        this.errorCode = INTERNAL_SERVER_ERROR;
+        this.baseError = INTERNAL_SERVER_ERROR;
     }
 
     // 에러 코드를 지정하는 생성자
-    public BaseException(Code errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+    public BaseException(BaseError baseError) {
+        super(baseError.getMessage());
+        this.baseError = baseError;
     }
 
     // 에러 코드와 메시지를 받는 생성자
-    public BaseException(Code errorCode, String message) {
+    public BaseException(BaseError baseError, String message) {
         super(message);
-        this.errorCode = errorCode;
+        this.baseError = baseError;
     }
 
     // 에러 코드, 메시지, 원인을 받는 생성자
-    public BaseException(Code errorCode, String message, Throwable cause) {
+    public BaseException(BaseError baseError, String message, Throwable cause) {
         super(message, cause);
-        this.errorCode = errorCode;
+        this.baseError = baseError;
     }
 
     // 에러 코드와 원인을 받는 생성자
-    public BaseException(Code errorCode, Throwable cause) {
+    public BaseException(BaseError baseError, Throwable cause) {
         super(cause);
-        this.errorCode = errorCode;
+        this.baseError = baseError;
     }
 }
