4e1538f37f1acc9b922fa65aa2ca7264d0d41223
yonghun <asqwklop12@gmail.com>
2025-04-09 14:43:00 +0900

    Chore: API명 수정

--- gathering-service/src/main/java/com/sparta/moim/member/presentation/controller/MemberController.java
+++ gathering-service/src/main/java/com/sparta/moim/member/presentation/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/gathering/recruit")
+@RequestMapping("/api/v1/gathering/member")
 @RequiredArgsConstructor
 public class MemberController {
   private final MemberService recruitService;
