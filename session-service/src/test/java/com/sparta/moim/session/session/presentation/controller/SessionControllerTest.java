f0f5bc0284a1c94378d3ea78dafa2d571d6a53c3
yonghun <asqwklop12@gmail.com>
2025-04-11 01:58:32 +0900

    Feat: 스웨거 연동

--- session-service/src/test/java/com/sparta/moim/session/session/presentation/controller/SessionControllerTest.java
+++ session-service/src/test/java/com/sparta/moim/session/session/presentation/controller/SessionControllerTest.java
@@ -1,7 +1,13 @@
 package com.sparta.moim.session.session.presentation.controller;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -10,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.moim.common.security.CustomUserDetails;
 import com.sparta.moim.session.session.application.dto.command.SearchSessionCommand;
@@ -29,14 +36,17 @@ import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+@AutoConfigureRestDocs
 @WebMvcTest(SessionController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class SessionControllerTest {
@@ -102,7 +112,36 @@ class SessionControllerTest {
         .andExpect(jsonPath("$.status").exists())
         .andExpect(jsonPath("$.applyTime").exists())
         .andExpect(jsonPath("$.openTime").exists())
-        .andExpect(jsonPath("$.closeTime").exists());
+        .andExpect(jsonPath("$.closeTime").exists())
+        .andDo(document("세션 - 생성",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 생성")
+                .description("세션을 생성하기 위한 엔드포인트입니다.")
+                .requestFields(
+                    fieldWithPath("organizationId").description("모임 아이디"),
+                    fieldWithPath("publisher").description("발표자"),
+                    fieldWithPath("title").description("발표 제목"),
+                    fieldWithPath("count").description("모집 인원"),
+                    fieldWithPath("openTime").description("오픈 시간"),
+                    fieldWithPath("closeTime").description("마감 시간"),
+                    fieldWithPath("applyInfo").description("신청 여부"),
+                    fieldWithPath("applyInfo.reason").description("신청 사유")
+                )
+                .responseFields(
+                    fieldWithPath("organizationId").description("모임 아이디"),
+                    fieldWithPath("sessionId").description("세션 아이디"),
+                    fieldWithPath("publisher").description("발표자"),
+                    fieldWithPath("title").description("세션 제목"),
+                    fieldWithPath("count").description("모집 인원"),
+                    fieldWithPath("status").description("모집 상태"),
+                    fieldWithPath("openTime").description("오픈 시간"),
+                    fieldWithPath("closeTime").description("마감 시간"),
+                    fieldWithPath("applyTime").description("싱청 시간"))
+                .build()
+            )));
   }
 
   @Test
@@ -143,7 +182,33 @@ class SessionControllerTest {
         .andExpect(jsonPath("$.publisher").value("test"))
         .andExpect(jsonPath("$.applyInfo.applyTime").exists())
         .andExpect(jsonPath("$.applyInfo.confirmTime").exists())
-        .andExpect(jsonPath("$.applyInfo.reason").value("test"));
+        .andExpect(jsonPath("$.applyInfo.reason").value("test"))
+        .andDo(document("세션 - 단일 조회",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 단일 조회")
+                .description("세션 단일조회 위한 엔드포인트입니다.")
+                .pathParameters(
+                    parameterWithName("sessionId").description("세션 아이디")
+                )
+                .responseFields(
+                    fieldWithPath("organizationId").description("모임 아이디"),
+                    fieldWithPath("sessionId").description("세션 아이디"),
+                    fieldWithPath("publisher").description("발표자"),
+                    fieldWithPath("title").description("세션 제목"),
+                    fieldWithPath("count").description("모집 인원"),
+                    fieldWithPath("status").description("모집 상태"),
+                    fieldWithPath("openTime").description("오픈 시간"),
+                    fieldWithPath("closeTime").description("마감 시간"),
+                    fieldWithPath("applyInfo").description("신청 정보"),
+                    fieldWithPath("applyInfo.applyTime").description("신청 시간"),
+                    fieldWithPath("applyInfo.confirmTime").description("승인 시간"),
+                    fieldWithPath("applyInfo.reason").description("신청 사유")
+                )
+                .build()
+            )));
   }
 
   @Test
@@ -163,7 +228,22 @@ class SessionControllerTest {
             .header("X-User-Name", "테스트유저")
             .header("X-User-Role", "USER")
             .header("X-User-ID", UUID.randomUUID().toString()))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(document("세션 - 수정",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 수정")
+                .description("세션을 수정하기 위한 엔드포인트입니다.")
+                .pathParameters(
+                    parameterWithName("sessionId").description("세션 아이디")
+                )
+                .requestFields(
+                    fieldWithPath("title").description("세션 제목"),
+                    fieldWithPath("count").description("모집 인원"))
+                .build()
+            )));
   }
 
   @Test
@@ -180,7 +260,21 @@ class SessionControllerTest {
             .header("X-User-Name", "테스트유저")
             .header("X-User-Role", "USER")
             .header("X-User-ID", UUID.randomUUID().toString()))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(document("세션 - 상태 변경",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 상태 변경")
+                .description("세션의 상태를 변경하기 위한 엔드포인트입니다.")
+                .pathParameters(
+                    parameterWithName("sessionId").description("세션 아이디")
+                )
+                .requestFields(
+                    fieldWithPath("status").description("세션 상태"))
+                .build()
+            )));
   }
 
 
@@ -202,7 +296,19 @@ class SessionControllerTest {
             .header("X-User-Name", "테스트유저")
             .header("X-User-Role", "USER")
             .header("X-User-ID", UUID.randomUUID().toString()))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(document("세션 - 삭제",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 삭제")
+                .description("세션삭제를 위한 엔드포인트입니다.")
+                .pathParameters(
+                    parameterWithName("sessionId").description("세션 아이디")
+                )
+                .build()
+            )));
   }
 
   @Test
@@ -216,7 +322,19 @@ class SessionControllerTest {
             .header("X-User-Name", "테스트유저")
             .header("X-User-Role", "USER")
             .header("X-User-ID", UUID.randomUUID().toString()))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(document("세션 - 승인",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 승인")
+                .description("세션 승인을 위한 엔드포인트입니다.")
+                .pathParameters(
+                    parameterWithName("sessionId").description("세션 아이디")
+                )
+                .build()
+            )));
   }
 
   @Test
@@ -262,6 +380,36 @@ class SessionControllerTest {
         .andExpect(jsonPath("$.sessions[0].publisher").value("publisher"))
         .andExpect(jsonPath("$.page").value(0))
         .andExpect(jsonPath("$.content").value(1))
-        .andExpect(jsonPath("$.total").value(1));
+        .andExpect(jsonPath("$.total").value(1))
+        .andDo(document("세션 - 검색",
+            preprocessRequest(Preprocessors.prettyPrint()),
+            preprocessResponse(Preprocessors.prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .tag("Session-External")
+                .summary("세션 검색")
+                .description("세션 검색을 위한 엔드포인트입니다.")
+//                .queryParameters(
+//                    parameterWithName("reason").description("승인 사유"),
+//                    parameterWithName("confirmTime").description("승인 시간"),
+//                    parameterWithName("sort").description("정렬 방법"),
+//                    parameterWithName("title").description("세션 제목"),
+//                    parameterWithName("isDeleted").description("삭제 여부"),
+//                    parameterWithName("size").description("가져올 데이터 수"),
+//                    parameterWithName("closeTime").description("마감 시간"),
+//                    parameterWithName("publisher").description("발표자"),
+//                    parameterWithName("page").description("페이지 수"),
+//                    parameterWithName("openTime").description("오픈 시간"),
+//                    parameterWithName("applyTime").description("신청 시간"),
+//                    parameterWithName("status").description("세션 상태")
+//                )
+                .responseFields(
+                    fieldWithPath("sessions").description("세션 리스트"),
+                    fieldWithPath("sessions[].title").description("세션 제목"),
+                    fieldWithPath("sessions[].publisher").description("발표자"),
+                    fieldWithPath("total").description("전체 갯수"),
+                    fieldWithPath("page").description("페이지 수"),
+                    fieldWithPath("content").description("현재 페이지에서 보여주는 아이템 수"))
+                .build()
+            )));
   }
 }
