2e43fe57660a545fad26e65040470f0d47742c8d
kim0527 <joo14753@naver.com>
2025-04-11 10:40:23 +0900

    Feat: Transactional readOnly 추가

--- post-service/src/main/java/com/moim/post/application/PostQueryService.java
+++ post-service/src/main/java/com/moim/post/application/PostQueryService.java
@@ -15,8 +15,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PostQueryService implements PostQueryUseCase {
 
