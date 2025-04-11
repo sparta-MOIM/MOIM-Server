3a7cd36262786371a3375f536d9bbc336a4e3ccc
yonghun <asqwklop12@gmail.com>
2025-04-11 17:12:09 +0900

    Feat: Session - Member 1:N 매핑

--- session-service/src/main/java/com/sparta/moim/session/member/domain/entity/Member.java
+++ session-service/src/main/java/com/sparta/moim/session/member/domain/entity/Member.java
@@ -4,17 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "p_session_member")
 public class Member {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
-  private Long sessionId;
 
   private String memberName;
 }
