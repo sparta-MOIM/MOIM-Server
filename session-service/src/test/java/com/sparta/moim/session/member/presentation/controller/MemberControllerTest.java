94961f04a22dd1106b450dee6e95e8b770a41f8f
yonghun <asqwklop12@gmail.com>
2025-04-11 23:14:20 +0900

    Test: 맴버 API 테스트 코드 작성

---
+++ session-service/src/test/java/com/sparta/moim/session/member/presentation/controller/MemberControllerTest.java
@@ -0,0 +1,125 @@
+package com.sparta.moim.session.member.presentation.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.moim.common.security.CustomUserDetails;
+import com.sparta.moim.session.member.application.MemberService;
+import com.sparta.moim.session.member.presentation.dto.request.RemoveMemberRequest;
+import com.sparta.moim.session.session.application.dto.result.CreateSessionResult;
+import com.sparta.moim.session.session.application.service.SessionService;
+import com.sparta.moim.session.session.domain.enums.SessionStatus;
+import com.sparta.moim.session.session.presentation.dto.request.CreateSessionApplyRequest;
+import com.sparta.moim.session.session.presentation.dto.request.CreateSessionRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MemberController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class MemberControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockitoBean
+  private MemberService memberService;
+
+  @Test
+  @DisplayName("세션 가입 성공")
+  void joinMember_success() throws Exception {
+    // given
+    UUID userId = UUID.randomUUID();
+    String username = "테스트유저";
+    String role = "USER";
+
+    CustomUserDetails customUserDetails = new CustomUserDetails(username, role, userId);
+    SecurityContextHolder.getContext().setAuthentication(
+        new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities())
+    );
+
+    UUID sessionId = UUID.randomUUID();
+
+    // when & then
+    mockMvc.perform(post("/api/v1/session/{sessionId}/join", sessionId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-User-Name", username)
+            .header("X-User-Role", role)
+            .header("X-User-ID", userId.toString()))
+        .andExpect(status().isOk());
+  }
+
+
+  @Test
+  @DisplayName("세션 나가기 성공")
+  void deleteMember_success() throws Exception {
+    // given
+    UUID userId = UUID.randomUUID();
+    String username = "테스트유저";
+    String role = "USER";
+
+    CustomUserDetails customUserDetails = new CustomUserDetails(username, role, userId);
+    SecurityContextHolder.getContext().setAuthentication(
+        new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities())
+    );
+
+    UUID sessionId = UUID.randomUUID();
+
+    // when & then
+    mockMvc.perform(delete("/api/v1/session/{sessionId}/leave", sessionId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-User-Name", username)
+            .header("X-User-Role", role)
+            .header("X-User-ID", userId.toString()))
+        .andExpect(status().isOk());
+  }
+
+
+  @Test
+  @DisplayName("세션 강퇴 성공")
+  void removeMember_success() throws Exception {
+    // given
+    UUID userId = UUID.randomUUID();
+    String username = "테스트유저";
+    String role = "USER";
+
+    CustomUserDetails customUserDetails = new CustomUserDetails(username, role, userId);
+    SecurityContextHolder.getContext().setAuthentication(
+        new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities())
+    );
+
+    UUID sessionId = UUID.randomUUID();
+
+    RemoveMemberRequest request = new RemoveMemberRequest(List.of("member1", "member2", "member3"));
+
+
+    // when & then
+    mockMvc.perform(delete("/api/v1/session/{sessionId}/remove", sessionId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request))
+            .header("X-User-Name", username)
+            .header("X-User-Role", role)
+            .header("X-User-ID", userId.toString()))
+        .andExpect(status().isOk());
+  }
+
+
+}
