6e4ed6e6804cdb3a35e840ca88dee38159a0918c
kim0527 <joo14753@naver.com>
2025-04-11 23:19:11 +0900

    Feat: Schedule create 관련 Request, Command, Response 생성

---
+++ schedule-service/src/main/java/com/moim/schedule/presentation/response/ScheduleResponse.java
@@ -0,0 +1,15 @@
+package com.moim.schedule.presentation.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ScheduleResponse(
+    UUID id,
+    UUID organizationId,
+    String title,
+    String content,
+    LocalDateTime start,
+    LocalDateTime end,
+    Integer totalVoter
+) {
+}
