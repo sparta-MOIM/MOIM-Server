c885340a12b7fe9f28098518c794633d06a8d272
yonghun <asqwklop12@gmail.com>
2025-04-12 20:29:56 +0900

    [Refactor] Session ExtraController ApiResponseData 적용

--- session-service/src/main/java/com/sparta/moim/session/session/presentation/controller/external/ExternalSessionController.java
+++ session-service/src/main/java/com/sparta/moim/session/session/presentation/controller/external/ExternalSessionController.java
@@ -1,5 +1,6 @@
 package com.sparta.moim.session.session.presentation.controller.external;
 
+import com.sparta.moim.common.response.ApiResponseData;
 import com.sparta.moim.common.security.CustomUserDetails;
 import com.sparta.moim.session.session.application.dto.DeleteSessionCommand;
 import com.sparta.moim.session.session.application.service.SessionService;
@@ -32,41 +33,45 @@ public class ExternalSessionController {
   private final SessionService sessionService;
 
   @PostMapping
-  public CreateSessionResponse createSession(@RequestBody @Valid CreateSessionRequest request,
-                                             @AuthenticationPrincipal CustomUserDetails details) {
-    return CreateSessionResponse.create(sessionService.createSession(request.toCommand(details.getUsername(), details.getRole())));
+  public ApiResponseData<CreateSessionResponse> createSession(@RequestBody @Valid CreateSessionRequest request,
+                                                             @AuthenticationPrincipal CustomUserDetails details) {
+    return ApiResponseData.success(CreateSessionResponse.create(sessionService.createSession(request.toCommand(details.getUsername(), details.getRole()))));
   }
 
 
   @GetMapping("/{sessionId}")
-  public GetSessionResponse getSession(@PathVariable UUID sessionId) {
-    return GetSessionResponse.get(sessionService.getSession(sessionId));
+  public ApiResponseData<GetSessionResponse> getSession(@PathVariable UUID sessionId) {
+    return ApiResponseData.success(GetSessionResponse.get(sessionService.getSession(sessionId)));
   }
 
   @GetMapping
-  public SearchSessionResponse searchSession(@ModelAttribute SearchSessionRequest request, @AuthenticationPrincipal CustomUserDetails details) {
-    return SearchSessionResponse.search(sessionService.searchSession(request.toCommand(details.getRole())));
+  public ApiResponseData<SearchSessionResponse> searchSession(@ModelAttribute SearchSessionRequest request, @AuthenticationPrincipal CustomUserDetails details) {
+    return ApiResponseData.success(SearchSessionResponse.search(sessionService.searchSession(request.toCommand(details.getRole()))));
   }
 
 
   @PutMapping("/{sessionId}")
-  public void updateSession(@PathVariable UUID sessionId, @RequestBody @Valid UpdateSessionRequest request) {
+  public ApiResponseData<Void> updateSession(@PathVariable UUID sessionId, @RequestBody @Valid UpdateSessionRequest request) {
     sessionService.updateSession(request.toCommand(sessionId));
+    return ApiResponseData.success(null);
   }
 
   @PatchMapping("/{sessionId}/status")
-  public void updateStateSession(@PathVariable UUID sessionId, @RequestBody @Valid UpdateStateRequest request) {
+  public ApiResponseData<Void> updateStateSession(@PathVariable UUID sessionId, @RequestBody @Valid UpdateStateRequest request) {
     sessionService.statusUpdateSession(request.toCommand(sessionId));
+    return ApiResponseData.success(null);
   }
 
   @DeleteMapping("/{sessionId}")
-  public void deleteSession(@PathVariable UUID sessionId, @AuthenticationPrincipal CustomUserDetails details) {
+  public ApiResponseData<Void> deleteSession(@PathVariable UUID sessionId, @AuthenticationPrincipal CustomUserDetails details) {
     sessionService.deleteSession(new DeleteSessionCommand(sessionId, details.getUsername()));
+    return ApiResponseData.success(null);
   }
 
   @PatchMapping("/{sessionId}/apply")
-  public void applySession(@PathVariable UUID sessionId) {
+  public ApiResponseData<Void> applySession(@PathVariable UUID sessionId) {
     sessionService.applySession(sessionId);
+    return ApiResponseData.success(null);
   }
 
 
