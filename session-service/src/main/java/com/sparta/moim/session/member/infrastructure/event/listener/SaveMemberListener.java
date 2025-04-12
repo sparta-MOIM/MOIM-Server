8797a3117334ca175cf5b56baa1029a8c635b056
yonghun <asqwklop12@gmail.com>
2025-04-12 16:07:41 +0900

    [Chore] 명칭 수정

--- session-service/src/main/java/com/sparta/moim/session/member/infrastructure/event/listener/SaveMemberListener.java
+++ session-service/src/main/java/com/sparta/moim/session/member/infrastructure/event/listener/SaveMemberListener.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class SaveMember {
+public class SaveMemberListener {
   private final MemberRepository memberRepository;
 
   @EventListener
