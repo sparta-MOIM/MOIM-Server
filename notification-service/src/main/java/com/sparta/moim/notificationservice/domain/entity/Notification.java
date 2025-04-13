3be7a238aceed260d2a34119d040cc5494cc906c
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-04-13 12:10:21 +0900

    feat : 알림 entity 작성

--- notification-service/src/main/java/com/sparta/moim/notificationservice/domain/entity/Notification.java
+++ notification-service/src/main/java/com/sparta/moim/notificationservice/domain/entity/Notification.java
@@ -1,11 +1,7 @@
 package com.sparta.moim.notificationservice.domain.entity;
 
-import com.sparta.moim.common.utils.BaseEntity;
-import com.sparta.moim.notificationservice.domain.enums.NotificationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,8 +23,8 @@ import org.hibernate.annotations.Where;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
-@Table(name = "p_notification_template")
-public class NotificationTemplate extends BaseEntity {
+@Table(name = "p_notification")
+public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,13 +35,16 @@ public class NotificationTemplate extends BaseEntity {
     @Column(name = "tracking_id", length = 36, nullable = false, unique = true)
     private UUID trackingId;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 50, unique = true)
-    private NotificationType notificationType;
+    @Column(name="notification_template_tracking_id", length = 36, nullable = false)
+    private UUID notificationTemplateTrackingId;
 
-    @Column(nullable = false)
-    private String title;
+    @Column(name="receiver_tracking_id", length = 36, nullable = false)
+    private UUID receiverTrackingId;
 
-    @Column
-    private String content;
+    @Column(name="sender_tracking_id", length = 36, nullable = false)
+    private UUID senderTrackingId;
+
+    @Column(name="is_read", nullable = false)
+    private boolean isRead;
+
 }
