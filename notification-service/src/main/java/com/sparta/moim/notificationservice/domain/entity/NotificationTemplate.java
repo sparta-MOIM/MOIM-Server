554d1410be37e09fcd334274f52b15baaa64be00
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-04-13 12:04:07 +0900

    feat : 알림 템플릿 엔티티 작성

---
+++ notification-service/src/main/java/com/sparta/moim/notificationservice/domain/entity/NotificationTemplate.java
@@ -0,0 +1,51 @@
+package com.sparta.moim.notificationservice.domain.entity;
+
+import com.sparta.moim.common.utils.BaseEntity;
+import com.sparta.moim.notificationservice.domain.enums.NotificationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Types;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Where(clause = "deleted_at IS NULL")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "p_notification_template")
+public class NotificationTemplate extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @UuidGenerator
+    @JdbcTypeCode(Types.VARCHAR)
+    @Column(name = "tracking_id", length = 36, nullable = false, unique = true)
+    private UUID trackingId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private NotificationType notificationType;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String content;
+}
