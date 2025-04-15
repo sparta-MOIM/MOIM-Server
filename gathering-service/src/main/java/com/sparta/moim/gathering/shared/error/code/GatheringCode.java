a695d98649a26d33e47312cf6e46f9412fc01fe6
yonghun <asqwklop12@gmail.com>
2025-04-16 08:28:43 +0900

    Refactor: 에러코드 수정사항 반영

--- gathering-service/src/main/java/com/sparta/moim/gathering/shared/error/code/GatheringCode.java
+++ gathering-service/src/main/java/com/sparta/moim/gathering/shared/error/code/GatheringCode.java
@@ -14,8 +14,8 @@ public enum GatheringCode implements Code {
   TIME_OUT_GATHERING(HttpStatus.BAD_REQUEST, "G003", "The time window to join this gathering has expired"),
   ALREADY_PARTICIPATE_GATHERING(HttpStatus.BAD_REQUEST, "G004", "You are already participating in this gathering"),
   NOT_OPEN_GATHERING(HttpStatus.BAD_REQUEST, "G005", "This gathering is not open"),
-  ROLE_NOT_ALLOWED_GATHERING(HttpStatus.BAD_REQUEST,"G006" , "You are not allowed to gather this role" ),
-  NOT_FOUND_GATHERING_MEMBER(HttpStatus.BAD_REQUEST,"G007" , "not found gathering member" ),
+  ROLE_NOT_ALLOWED_GATHERING(HttpStatus.BAD_REQUEST,"G006", "You are not allowed to gather this role"),
+  NOT_FOUND_GATHERING_MEMBER(HttpStatus.NOT_FOUND,"G007", "The gathering member could not be found"),
   ;
 
 
