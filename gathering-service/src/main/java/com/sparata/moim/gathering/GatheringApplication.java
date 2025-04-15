a0f27b9f9b5da62b9a54b76f9aebf0b7a19451dd
yonghun <asqwklop12@gmail.com>
2025-04-15 17:15:48 +0900

    Chore: 유레카 등록

--- gathering-service/src/main/java/com/sparata/moim/gathering/GatheringApplication.java
+++ gathering-service/src/main/java/com/sparata/moim/gathering/GatheringApplication.java
@@ -2,10 +2,12 @@ package com.sparata.moim.gathering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class GatheringApplication {
 
 	public static void main(String[] args) {
