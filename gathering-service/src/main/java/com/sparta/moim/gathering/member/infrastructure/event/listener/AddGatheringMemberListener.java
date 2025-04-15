20c8768b53d8aa11ff336ba7ef07c2c63605629f
yonghun <asqwklop12@gmail.com>
2025-04-15 19:06:02 +0900

    Refactor: 로그메시지 개선

--- gathering-service/src/main/java/com/sparta/moim/gathering/member/infrastructure/event/listener/AddGatheringMemberListener.java
+++ gathering-service/src/main/java/com/sparta/moim/gathering/member/infrastructure/event/listener/AddGatheringMemberListener.java
@@ -6,6 +6,7 @@ import com.sparta.moim.gathering.shared.dto.SharedGatheringMember;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +19,14 @@ public class AddGatheringMemberListener {
   @Transactional
   public void save(SharedGatheringMember sharedGatheringMember) {
     try {
-      log.info("Saving publisher {}", sharedGatheringMember.memberName());
+      log.info("소모임 멤버 저장 시작: {}", sharedGatheringMember.memberName());
       memberRepository.save(Member.from(sharedGatheringMember));
-      log.info("Successfully saved publisher {}", sharedGatheringMember.memberName());
-    } catch (Exception e) {
-      log.error("Failed to save publisher {}: {}", sharedGatheringMember.memberName(), e.getMessage(), e);
+      log.info("소모임 멤버 저장 완료: {}", sharedGatheringMember.memberName());
+    } catch (DataIntegrityViolationException e) {
+      log.warn("멤버 저장 중복 오류: {}: {}", sharedGatheringMember.memberName(), e.getMessage());
+        // 중복 데이터인 경우 별도 처리 로직
+    }  catch (RuntimeException e) {
+      log.error("소모임 멤버 저장 실패: {}: {}", sharedGatheringMember.memberName(), e.getMessage(), e);
       //TODO 보상 트랜잭션 적용
     }
   }
