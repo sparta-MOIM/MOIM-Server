187e18a453eb3c6b02f0d407f09799cb12988e5f
kim0527 <joo14753@naver.com>
2025-04-15 15:53:40 +0900

    Build: Feign Client 등록

--- schedule-service/src/main/java/com/moim/schedule/ScheduleApplication.java
+++ schedule-service/src/main/java/com/moim/schedule/ScheduleApplication.java
@@ -2,7 +2,9 @@ package com.moim.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ScheduleApplication {
 
