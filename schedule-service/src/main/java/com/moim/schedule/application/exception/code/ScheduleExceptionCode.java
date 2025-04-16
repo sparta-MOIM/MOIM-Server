14dfe050ff6e93c897dc3ece10b35ed1f0f42a15
kim0527 <joo14753@naver.com>
2025-04-16 14:34:40 +0900

    Chore: 에러 코드 수정

--- schedule-service/src/main/java/com/moim/schedule/application/exception/code/ScheduleExceptionCode.java
+++ schedule-service/src/main/java/com/moim/schedule/application/exception/code/ScheduleExceptionCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ScheduleExceptionCode implements Code {
 
-  NOT_FOUND_SCHEDULE(HttpStatus.BAD_REQUEST, "SCG001" , "일정을 찾을 수 없습니다.");
+  NOT_FOUND_SCHEDULE(HttpStatus.NOT_FOUND, "SCG001" , "일정을 찾을 수 없습니다.");
 
   private final HttpStatus status;
   private final String code;
