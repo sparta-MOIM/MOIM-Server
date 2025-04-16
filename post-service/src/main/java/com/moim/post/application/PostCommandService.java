f718cba225aa10000e0e2c1974478c38a031bcc6
kim0527 <joo14753@naver.com>
2025-04-16 16:32:25 +0900

    Feat: Role 검증을 위한 Validator 적용

--- post-service/src/main/java/com/moim/post/application/PostCommandService.java
+++ post-service/src/main/java/com/moim/post/application/PostCommandService.java
@@ -5,12 +5,17 @@ import com.moim.post.application.command.CreateVoteCommand;
 import com.moim.post.application.command.DeleteCommand;
 import com.moim.post.application.command.UpdateFeedCommand;
 import com.moim.post.application.command.UpdateVoteCommand;
+import com.moim.post.application.exception.NotFoundFeed;
+import com.moim.post.application.exception.NotFoundVote;
+import com.moim.post.application.exception.PostUnauthorizedAccessException;
 import com.moim.post.application.usecase.PostCommandUseCase;
+import com.moim.post.application.validation.RoleValidator;
 import com.moim.post.domain.feed.Feed;
 import com.moim.post.domain.repository.command.FeedCommandRepository;
 import com.moim.post.domain.repository.command.VoteCommandRepository;
 import com.moim.post.domain.repository.command.entitymanager.FeedEntityManager;
 import com.moim.post.domain.vote.Vote;
+import com.sparta.moim.common.security.CustomUserDetails;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,9 +29,14 @@ public class PostCommandService implements PostCommandUseCase {
   private final FeedCommandRepository feedRepository;
   private final VoteCommandRepository voteRepository;
   private final FeedEntityManager feedEntityManager;
+  private final RoleValidator roleValidator;
 
   @Override
-  public Feed createFeed(CreateFeedCommand command) {
+  public Feed createFeed(
+      CustomUserDetails userDetails,
+      CreateFeedCommand command
+  ) {
+    checkRole("CREATE", command.organizationId(), userDetails.getTrackingId());
     Feed feed = Feed.create(
         command.organizationId(),
         command.title(),
@@ -38,7 +48,11 @@ public class PostCommandService implements PostCommandUseCase {
   }
 
   @Override
-  public Vote createVote(CreateVoteCommand command) {
+  public Vote createVote(
+      CustomUserDetails userDetails,
+      CreateVoteCommand command
+  ) {
+    checkRole("CREATE", command.organizationId(), userDetails.getTrackingId());
     Vote vote = Vote.create(
         command.organizationId(),
         command.title(),
@@ -51,9 +65,13 @@ public class PostCommandService implements PostCommandUseCase {
   }
 
   @Override
-  public Feed updateFeed(UUID id, UpdateFeedCommand command) {
-    // todo : 예외처리
-    Feed feed = feedRepository.findByTrackingId(id).orElseThrow(null);
+  public Feed updateFeed(
+      CustomUserDetails userDetails,
+      UUID id,
+      UpdateFeedCommand command
+  ) {
+    checkRole("UPDATE", command.organizationId(), userDetails.getTrackingId());
+    Feed feed = feedRepository.findByTrackingId(id).orElseThrow(NotFoundFeed::new);
     command.title().ifPresent(feed::updateTitle);
     command.content().ifPresent(feed::updateContent);
     command.imageUrl().ifPresent(feed::updateImageUrl);
@@ -65,9 +83,13 @@ public class PostCommandService implements PostCommandUseCase {
   }
 
   @Override
-  public Vote updateVote(UUID id, UpdateVoteCommand command) {
-    // todo : 예외처리
-    Vote vote = voteRepository.findByTrackingId(id).orElseThrow(null);
+  public Vote updateVote(
+      CustomUserDetails userDetails,
+      UUID id,
+      UpdateVoteCommand command
+  ) {
+    checkRole("UPDATE", command.organizationId(), userDetails.getTrackingId());
+    Vote vote = voteRepository.findByTrackingId(id).orElseThrow(NotFoundVote::new);
     command.title().ifPresent(vote::updateTitle);
     command.content().ifPresent(vote::updateContent);
     command.start().ifPresent(vote::updateStart);
@@ -77,18 +99,36 @@ public class PostCommandService implements PostCommandUseCase {
   }
 
   @Override
-  public void deleteFeed(DeleteCommand command) {
-    // todo: 예외처리
-    Feed feed = feedRepository.findByTrackingId(command.id()).orElseThrow(null);
+  public void deleteFeed(
+      CustomUserDetails userDetails,
+      DeleteCommand command
+  ) {
+    checkRole("DELETE", command.organizationId(), userDetails.getTrackingId());
+    Feed feed = feedRepository.findByTrackingId(command.postId()).orElseThrow(NotFoundFeed::new);
     feed.delete();
     // todo: 댓글 도메인에 삭제 이벤트 발행
   }
 
   @Override
-  public void deleteVote(DeleteCommand command) {
-    // todo: 예외처리
-    Vote vote = voteRepository.findByTrackingId(command.id()).orElseThrow(null);
+  public void deleteVote(
+      CustomUserDetails userDetails,
+      DeleteCommand command
+  ) {
+    checkRole("DELETE", command.organizationId(), userDetails.getTrackingId());
+    Vote vote = voteRepository.findByTrackingId(command.postId()).orElseThrow(NotFoundFeed::new);
     vote.delete();
   }
+
+  private void checkRole(
+      String requestType,
+      UUID organizationTrackingId,
+      UUID userTrackingId
+  ) {
+    Boolean result = roleValidator.isValidRole(requestType, organizationTrackingId, userTrackingId);
+    if (!result) {
+      throw new PostUnauthorizedAccessException();
+    }
+  }
+
 }
 
