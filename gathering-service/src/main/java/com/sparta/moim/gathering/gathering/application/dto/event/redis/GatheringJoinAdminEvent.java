1f8bd50fb1f4ce307a1b131b9268463c09bb8e24
yonghun <asqwklop12@gmail.com>
2025-04-22 01:39:50 +0900

    Refactor: 하드코딩된값 상수로 관리하도록 변경

--- gathering-service/src/main/java/com/sparta/moim/gathering/gathering/application/dto/event/redis/GatheringJoinAdminEvent.java
+++ gathering-service/src/main/java/com/sparta/moim/gathering/gathering/application/dto/event/redis/GatheringJoinAdminEvent.java
@@ -1,5 +1,6 @@
 package com.sparta.moim.gathering.gathering.application.dto.event.redis;
 
+import com.sparta.moim.gathering.shared.enums.MemberType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -11,23 +12,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class GatheringJoinAdminEvent {
+  private static final String KEY_GATHERING_ID = "gathering_id";
+  private static final String KEY_MEMBER_NAME = "member_name";
+  private static final String KEY_TYPE = "type";
+
   private UUID gatheringId;
   private String memberName;
-  private String type;
+  private MemberType type;
 
   public Map<String, String> toMap() {
     Map<String, String> map = new HashMap<>();
-    map.put("gathering_id", gatheringId.toString());
-    map.put("member_name", memberName);
-    map.put("type", type);
+    map.put(KEY_GATHERING_ID, gatheringId.toString());
+    map.put(KEY_MEMBER_NAME, memberName);
+    map.put(KEY_TYPE, type.name());
     return map;
   }
 
   public static GatheringJoinAdminEvent fromMap(Map<String, String> map) {
     return new GatheringJoinAdminEvent(
-        UUID.fromString(map.get("gathering_id")),
-        map.get("member_name"),
-        map.get("type")
-    );
+        UUID.fromString(map.get(KEY_GATHERING_ID)),
+        map.get(KEY_MEMBER_NAME),
+        MemberType.valueOf(map.get(KEY_TYPE)));
   }
 }
