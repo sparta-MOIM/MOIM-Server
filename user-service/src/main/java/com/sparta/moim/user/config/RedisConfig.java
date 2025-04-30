0d2a4c2446716c66aa68ff54424f232e9515ae4d
cchoijjinyoung <cgy7127@gmail.com>
2025-05-01 02:08:16 +0900

    Feat: Redis 비밀번호 추가

--- user-service/src/main/java/com/sparta/moim/user/config/RedisConfig.java
+++ user-service/src/main/java/com/sparta/moim/user/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -17,8 +18,19 @@ public class RedisConfig {
   @Value("${spring.data.redis.port}")
   private int redisPort;
 
+  @Value("${spring.data.redis.password}")
+  private String password;
+
+  /**
+   * RedisConnectionFactory는 스프링 어플리케이션과 레디스를 연결하기 위해 사용된다. 커넥션의 종류로는 Jedis와 Lettuce가 있는데, Lettuce의 성능이 더 좋은 것으로 알려져있다.
+   */
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(redisHost, redisPort);
+  public RedisConnectionFactory redisConnectionFactory() {// redis 연결 정보(host, port, password)를 지정한다.
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+        redisHost, redisPort);
+    redisStandaloneConfiguration.setPassword(password);
+
+    // redis 연결 정보를 토대로 LettuceConnectionFactory 객체를 생성하여 빈으로 등록한다.
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
   }
 }
