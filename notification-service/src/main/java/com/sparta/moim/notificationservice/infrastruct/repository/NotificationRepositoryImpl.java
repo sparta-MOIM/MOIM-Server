599436a48b8f7c9ca914c3acff8c69b7ed9d9125
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-05-04 18:09:37 +0900

    feat : batch insert 쿼리 로직 작성

--- notification-service/src/main/java/com/sparta/moim/notificationservice/infrastruct/repository/NotificationRepositoryImpl.java
+++ notification-service/src/main/java/com/sparta/moim/notificationservice/infrastruct/repository/NotificationRepositoryImpl.java
@@ -3,6 +3,10 @@ package com.sparta.moim.notificationservice.infrastruct.repository;
 import com.sparta.moim.common.page.Pagination;
 import com.sparta.moim.notificationservice.domain.entity.Notification;
 import com.sparta.moim.notificationservice.domain.repository.NotificationRepository;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,11 +23,43 @@ import org.springframework.stereotype.Repository;
 public class NotificationRepositoryImpl implements NotificationRepository {
 
     private final NotificationJpaRepository notificationJpaRepository;
+    private final JdbcTemplate jdbcTemplate;
 
 
     @Override
     public void saveAll(List<Notification> notificationList) {
-        notificationJpaRepository.saveAll(notificationList);
+        String sql = "INSERT INTO p_notification ("
+                + "tracking_id, notification_type, access_tracking_id, receiver_tracking_id, content, is_read, "
+                + "created_at, created_by, modified_at, modified_by) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        jdbcTemplate.batchUpdate(sql,
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        Notification notification = notificationList.get(i);
+                        UUID trackingId = UUID.randomUUID();
+                        LocalDateTime createdAt = LocalDateTime.now();
+                        LocalDateTime modifiedAt = LocalDateTime.now();
+
+                        ps.setString(1, trackingId.toString());
+                        ps.setString(2, notification.getNotificationType().name());
+                        ps.setString(3, notification.getAccessTrackingId().toString());
+                        ps.setString(4, notification.getReceiverTrackingId().toString());
+                        ps.setString(5, notification.getContent());
+                        ps.setBoolean(6, false); // 읽지 않음
+                        ps.setTimestamp(7, Timestamp.valueOf(createdAt));
+                        ps.setString(8, "System"); // 생성자
+                        ps.setTimestamp(9, Timestamp.valueOf(modifiedAt));
+                        ps.setString(10, "System"); // 수정자
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return notificationList.size();
+                    }
+            }
+        );
     }
 
     @Override
