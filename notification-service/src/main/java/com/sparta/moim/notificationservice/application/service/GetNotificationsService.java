c7cf31d4cba4746077a9591815a3c24034295504
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-05-18 23:19:40 +0900

    fix : 날짜 테스트 수정

--- notification-service/src/main/java/com/sparta/moim/notificationservice/application/service/GetNotificationsService.java
+++ notification-service/src/main/java/com/sparta/moim/notificationservice/application/service/GetNotificationsService.java
@@ -8,7 +8,9 @@ import com.sparta.moim.notificationservice.application.util.PaginationMap;
 import com.sparta.moim.notificationservice.domain.entity.Notification;
 import com.sparta.moim.notificationservice.domain.repository.NotificationRepository;
 import com.sparta.moim.notificationservice.presentation.dto.GetNotificationResponse;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,8 +25,14 @@ public class GetNotificationsService implements GetNotificationsUseCase {
     private final static int NOTIFICATION_DURATION = 14; // 최근 14일 알림만 조회
     @Override
     public Pagination<GetNotificationResponse> execute(String userTrackingId, Boolean isRead, int page, int size) {
+        // 2025-05-17 날짜를 LocalDate로 변환. 테스트를 위해 진행 ( todo -추후삭제)
+        String dateStr = "2025-05-31";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        LocalDateTime startDate = LocalDateTime.now().minusDays(NOTIFICATION_DURATION);
+        LocalDateTime startDate = LocalDate.parse(dateStr, formatter)
+                .atStartOfDay()
+                .minusDays(NOTIFICATION_DURATION);
+
         Pagination<Notification> notifications = notificationRepository.findByUserTrackingIdAndIsRead(
                 userTrackingId, isRead, startDate,page, size);
         Pagination<GetNotificationQuery> getNotificationQueryPagination = PaginationMap.map(notifications, GetNotificationQuery::from);
